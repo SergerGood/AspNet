655a9377b0b2e0e5747a5e3f20c4b612d1778b9e
SergerGood <serzher@inbox.ru>
2016-01-25 11:06:27 +0300

    javascript events

--- Source/TheWorld/wwwroot/js/site.js
+++ Source/TheWorld/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 
 var ele = document.getElementById("username");
 ele.innerHTML = "Shawn";
+
+var main = document.getElementById("main");
+
+main.onmouseenter = function () {
+    main.style["background-color"] = "#888;";
+};
+
+main.onmouseleave = function() {
+    main.style["background-color"] = "";
+}
