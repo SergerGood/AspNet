46b9f1cb19052a0a1d297f83103c65eff25daedb
SergerGood <serzher@inbox.ru>
2016-01-26 21:24:48 +0300

    introducing jQuery

--- Source/TheWorld/wwwroot/js/site.js
+++ Source/TheWorld/wwwroot/js/site.js
@@ -1,15 +1,23 @@
 ﻿//site.js
-(function() {
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Shawn";
+(function () {
 
-    var main = document.getElementById("main");
+    var ele = $("#username");
+    ele.text("Shawn1");
 
-    main.onmouseenter = function() {
+    var main = $("#main");
+
+    main.on("mouseenter", function() {
         main.style.backgroundColor = "#888";
-    };
+    });
 
-    main.onmouseleave = function() {
+    main.on("mouseleave", function () {
         main.style.backgroundColor = "";
-    }
+    });
+
+    var menuItems = $("ul.menu li a");
+    menuItems.on("click", function () {
+        var me = $(this);
+        alert(me.text())
+    });
+
 })();
