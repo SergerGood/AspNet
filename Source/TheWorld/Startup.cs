5c77893a7be5ef0b1bf4906ccc21afbc4878d0f2
SergerGood <serzher@inbox.ru>
2016-01-29 22:13:25 +0300

    dbcontext

--- Source/TheWorld/Startup.cs
+++ Source/TheWorld/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.PlatformAbstractions;
 using TheWorld.Services;
+using TheWorld.Models;
 
 namespace TheWorld
 {
@@ -43,6 +44,9 @@ namespace TheWorld
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<WorldContext>();
 
 #if DEBUG
             services.AddScoped<IMailService, DebugMailService>();
