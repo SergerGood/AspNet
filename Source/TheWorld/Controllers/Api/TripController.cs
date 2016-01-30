cd109a84bc7104803067aff936d5f080bc6453cc
SergerGood <serzher@inbox.ru>
2016-01-30 20:20:09 +0300

    create an api controller

---
+++ Source/TheWorld/Controllers/Api/TripController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace TheWorld.Controllers.Api
+{
+    public class TripController : Controller
+    {
+        [HttpGet("api/trips")]
+        public JsonResult Get()
+        {
+            return Json( new { name = "Shawn" });
+        }
+    }
+}
