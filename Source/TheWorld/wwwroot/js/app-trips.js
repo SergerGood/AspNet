be1d042b6b7ca7e590b9babe3ed7f1acfe2c7dfa
SergerGood <serzher@inbox.ru>
2016-02-03 21:02:01 +0300

    add routing

--- Source/TheWorld/wwwroot/js/app-trips.js
+++ Source/TheWorld/wwwroot/js/app-trips.js
@@ -2,6 +2,19 @@
 
     "use strict";
 
-    angular.module("app-trips", ["simpleControls"]);
+    angular.module("app-trips", ["simpleControls", "ngRoute"])
+        .config(function ($routeProvider) {
+
+            $routeProvider.when("/", {
+                controller: "tripController",
+                controllerAs: "vm",
+                templateUrl: "/views/tripsView.html"
+            });
+
+            $routeProvider.otherwise({
+                redirectTo: "/"
+            })
+
+        });
 
 })();
