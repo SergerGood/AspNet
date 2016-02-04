a66b95fb67d9c5719b83e89f4a4170879003abc3
SergerGood <serzher@inbox.ru>
2016-02-04 21:37:31 +0300

    creating a new stop

--- Source/TheWorld/wwwroot/js/tripEditorController.js
+++ Source/TheWorld/wwwroot/js/tripEditorController.js
@@ -13,7 +13,9 @@
         vm.isBusy = true;
         vm.newStop = {};
 
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        var url = "/api/trips/" + vm.tripName + "/stops";
+
+        $http.get(url)
             .then(function (response) {
 
                 angular.copy(response.data, vm.stops);
@@ -25,6 +27,25 @@
             .finally(function () {
                 vm.isBusy = false;
             });
+
+        vm.addStop = function () {
+
+            vm.isBusy = true;
+
+            $http.post(url, vm.newStop)
+                .then(function (response) {
+
+                    vm.stops.push(response.data);
+                    _showMap(vm.stops);
+                    vm.newStop = {};
+
+                }, function (err) {
+                    vm.errorMessage = "Failed to add new stop";
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
+        };
     };
 
     function _showMap(stops) {
